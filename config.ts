b0d1ad9726b0e68efab66a69cf1b38b12c457784
PlazmoCraftServer <plazmocraft@gmail.com>
2025-08-31 13:45:56 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,16 +1,16 @@
 const publicKey = '-----BEGIN PUBLIC KEY-----\n' +
-    'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA8FhVppuhFW7Kqn1sOCY4\n' +
-    'Qd8ttTY5BbCCElTbuNmAmqp9kW4V3OofqXZK5Z3FJOsi0iiW4Sep5ehc7FplQqVV\n' +
-    'pqGmCiaOqW90NGnzZSs5OMiGPM1zCg3bUqtfYXbmaJIipAVnK/F92wR07pSWoQBb\n' +
-    'aghQGlRsbQq0vQLL7U9JbB2kOhXDiIHBQPfHE0ixAxeDevTYbBvlfRr6f40c3YTH\n' +
-    '4nD9WA8+aChw5dt9IVPzYdrXlTAdV3x2/1KIYxMSHi15AJqBOeZZCxrBTEgqlRUp\n' +
-    'KTaWSTR+QE/4my/qjm3HrNmNYplpny3BjvfG74g9InCY2zWnxryA0+gtzhv7CxJM\n' +
-    'CW3F1WD4DKAhG2s4SeA3C7JwN+k+AwGLciD47eHuXvN36RKuiKAGXMUj1QfR0Nd4\n' +
-    'znrXe0QAJCJtSk1m1EwFeOZLTumOLGpgoiieEVKqEvkjR7msyYG7Se55OYw+Aa+J\n' +
-    'FET1ZMtpilqGeX/CxFrUZtXQk8h5OM5JmLA2Zm6oeCVU1nISgEzBURxS/kAMpOil\n' +
-    'T6/kSRHwRE0U9UZLSNR0eY2USatz52z0ND6SeIH6G7zZl+8P3lafloN0g+hrMeIn\n' +
-    'h0dUqNFJe7D/e+OnRgd8S9u0k1dbfRWQyA/1lcl+m7B43Xu/XVsHKSjGgjeNq64q\n' +
-    'TVXHSy3OmW64nEUVMoNdkskCAwEAAQ==\n' +
+    'MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA34H6UXnhIQf6qYy7BUxY\n' +
+    'lk0WDhqmliNx2r0OulmYv9+XXRruNZopirZqS3FW5kdA8YcxnNDdWEXXGgFChX0L\n' +
+    'VKFRoQxQ/euSgJJKPmvYtinON3apenWMT8lKG4ADG00nezJD7qxqyPzt2dwNn6xC\n' +
+    '6ESEi9CqmXY40YMn7hyA+EJRATGtzEzRcYx6Sz90In0zmyT5VYrlMHw2y7RHlwSY\n' +
+    'K/CUq9JiBPNXEQVKOGB/CXl2Wqk91MjDGolEXmUxnyOKAlr6/3PTWtz300ZVzHOr\n' +
+    'hmxnSUWy+RivVQMwjTesYcR6/WR9BqFHNCTjYLvinK/hxxVOMEkaX2DoKBrI/zeh\n' +
+    '5IRy8ZDo8GlLNoaN1u2oSBpCdHOAtMTG8iZtxPaDqflYeZ3mUp1TOWksxgYRFHFr\n' +
+    '8031i7RUHf7AaZcobgNym4SYXjgvQHGiuqiKxDA8isBPmbJ73fTsTyZxSBgdOr/A\n' +
+    'q8i98RjmbBIElbQKaowR03mkVMFVaoI25xBkTweaKQ+iAakDWUo2MumbWttsafdA\n' +
+    '88SnDDHLWcRUItLdIe1LL4tmOYwHbxscBihvtP17NBltNviB5uvAggKLCY82DkDj\n' +
+    'zwm/4NemtovVMhWbjomz0AYilvZSHWx/BUvbuTJyCo0b5gdGKJOlAbL024iE9RbT\n' +
+    '8T2ocOPZgYSnOfHu+vVDEZ8CAwEAAQ==\n' +
     '-----END PUBLIC KEY-----\n';
 
 export const window = {
@@ -24,8 +24,8 @@ export const window = {
 };
 
 export const api = {
-    ws: 'ws://107.161.154.242:1370/ws',
-    web: 'http://107.161.154.242:1370',
+    ws: 'ws://64.188.76.52:1370/ws',
+    web: 'http://64.188.76.52:1370',
     publicKey
 };
 
